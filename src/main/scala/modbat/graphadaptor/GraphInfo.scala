b9bc88b191a71752b1610a922c56eda16584f55a
unknown <georgewba@gmail.com>
2021-03-09 00:44:36 +0100

    add comments

--- src/main/scala/modbat/graphadaptor/GraphInfo.scala
+++ src/main/scala/modbat/graphadaptor/GraphInfo.scala
@@ -19,6 +19,8 @@ class StateData(val state: State) {
     } else {
       val that: StateData = other.asInstanceOf[StateData]
 
+      // TODO GR_NR: either the state reference is compared (globally) or
+      // state names locally
       this.state.name.equals(that.state.name)
     }
   }
@@ -39,6 +41,7 @@ class EdgeData(val transitionLabel: String, val transitionType: TransitionType, 
     } else {
       val that: EdgeData = other.asInstanceOf[EdgeData]
 
+      // TODO GR_NR:  within the model, transition-id is unique globally
       this.transitionId.equals(that.transitionId)
     }
   }
